25494419701434da64e6ad0cffc8b444ea2c8d07
Unknown <thomas.kammann@study.hs-duesseldorf.de>
2018-03-03 15:36:34 +0100

    Note Functions ADD/LIST/READ/REMOVE

--- practice/lesson1/app.js
+++ practice/lesson1/app.js
@@ -2,20 +2,28 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv =  yargs.argv;
+
 var command = process.argv[2];
 console.log(`Command:`, command);
+console.log(`Node Argv:`, process.argv);
+console.log(`Yarg Argv:`, argv);
 
 if(command == 'add') {
   console.log('Adding Note.')
-  notes.addNote();
+  notes.addNote(argv.title, argv.body);
 } else if(command == 'list') {
   console.log('Listing All Notes.')
+  notes.getAll();
 } else if(command == 'read') {
+  notes.getNote(argv.title);
   console.log('Reading Specific Note')
 } else if(command == 'remove') {
+  notes.removeNote(argv.title);
   console.log('Remove Specific Note')
 } else {
   console.log('Cant find Command.')
--- practice/lesson1/notes.js
+++ practice/lesson1/notes.js
@@ -1,6 +1,28 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = function() {
-  console.log('addNote');
-  return 'New Note';
+var addNote = function(title, body) {
+  console.log('Adding Note:', title, body);
 };
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting Note:', title);
+};
+
+var removeNote = (title) => {
+  console.log('Remove Note:', title);
+};
+
+module.exports = {
+  //addNote: addNote
+  addNote,
+  //getAll: getAll
+  getAll,
+  //getNote: getNote
+  getNote,
+  //removeNote: removeNote
+  removeNote
+}
